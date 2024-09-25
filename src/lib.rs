2cd66fa9ad5c151f474bd3a761cbc90e4753cbf3
Andy Allison <rustacean15@gmail.com>
2024-09-25 00:00:22 -0500

    add struct examples

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,7 @@
 /*! Derive-macro-based generator that combines `Typestate` and `Builder` patterns. */
 
 #[cfg(test)]
+#[allow(dead_code)]
 mod tests {
     use std::{collections::HashMap, marker::PhantomData};
 
@@ -113,114 +114,157 @@ mod tests {
         }
     }
 
-    #[test]
-    fn builder_typestate() {
-        #[derive(Debug)]
-        struct Person {
-            name: String,
-            age: u32,
-            email: Option<String>,
+    #[derive(TypestateBuilder)]
+    struct MultiBoundGeneric<T, U>
+    where
+        T: Clone + Default + std::fmt::Debug,
+        U: Into<String> + Copy,
+    {
+        item1: T,
+        item2: U,
+        data: Vec<T>,
+    }
+
+    trait Processor {
+        type Input;
+        type Output;
+
+        fn process(&self, input: Self::Input) -> Self::Output;
+    }
+
+    #[derive(TypestateBuilder)]
+    struct StructWithAssociatedTypes<T>
+    where
+        T: Processor,
+    {
+        processor: T,
+        input: T::Input,
+    }
+
+    trait Container {
+        type Item;
+
+        fn add_item(&mut self, item: Self::Item);
+        fn get_item(&self) -> &Self::Item;
+    }
+
+    #[derive(TypestateBuilder)]
+    struct GenericContainer<T>
+    where
+        T: Container,
+    {
+        container: T,
+    }
+
+    #[derive(TypestateBuilder)]
+    struct AdvancedPhantomStruct<'a, T>
+    where
+        T: 'a + Clone,
+    {
+        reference: &'a T,
+        marker: PhantomData<T>,
+    }
+
+    trait Drawable {
+        fn draw(&self);
+    }
+
+    #[derive(TypestateBuilder)]
+    struct GenericWithTraitObject<'a, T>
+    where
+        T: 'a + Drawable,
+    {
+        drawable_item: Box<dyn Drawable + 'a>,
+        generic_item: T,
+    }
+
+    #[derive(TypestateBuilder)]
+    struct StructWithFunctionPointer<T, F>
+    where
+        F: Fn(T) -> T,
+    {
+        func: F,
+        value: T,
+    }
+    trait Graph {
+        type Node<'a>
+        where
+            Self: 'a;
+        type Edge<'a>
+        where
+            Self: 'a;
+
+        fn get_node<'a>(&'a self) -> Self::Node<'a>;
+        fn get_edge<'a>(&'a self) -> Self::Edge<'a>;
+    }
+
+    #[derive(TypestateBuilder)]
+    struct GraphStruct<T>
+    where
+        T: Graph,
+    {
+        graph: T,
+    }
+
+    impl<T> GraphStruct<T>
+    where
+        T: Graph,
+    {
+        fn display_node<'a>(&self) {
+            let node = self.graph.get_node();
+            // Implement some logic to display the node
+        }
+    }
+
+    #[derive(TypestateBuilder)]
+    struct ArrayWrapper<T, const N: usize> {
+        items: [T; N],
+    }
+
+    impl<T, const N: usize> ArrayWrapper<T, N> {
+        fn new(items: [T; N]) -> Self {
+            ArrayWrapper { items }
         }
 
-        #[derive(Debug)]
-        struct PersonBuilderNameAdded(String);
-        #[derive(Debug)]
-        struct PersonBuilderNameEmpty;
-        #[derive(Debug)]
-        struct PersonBuilderAgeAdded(u32);
-        #[derive(Debug)]
-        struct PersonBuilderAgeEmpty;
-        #[derive(Debug)]
-        struct PersonBuilderEmailAdded(Option<String>);
-        #[derive(Debug)]
-        struct PersonBuilderEmailEmpty;
-
-        #[derive(Debug)]
-        struct PersonBuilder<Name, Age, Email> {
-            name: Name,
-            age: Age,
-            email: Email,
+        fn get_length(&self) -> usize {
+            N
         }
+    }
 
-        impl Person {
-            fn builder(
-            ) -> PersonBuilder<PersonBuilderNameEmpty, PersonBuilderAgeEmpty, PersonBuilderEmailEmpty>
-            {
-                PersonBuilder {
-                    name: PersonBuilderNameEmpty,
-                    age: PersonBuilderAgeEmpty,
-                    email: PersonBuilderEmailEmpty,
-                }
-            }
+    trait Action {
+        fn execute(&self);
+    }
+
+    #[derive(TypestateBuilder)]
+    struct Dispatcher<T: Action> {
+        handler: Box<dyn Action>,
+        generic_handler: T,
+    }
+
+    impl<T: Action> Dispatcher<T> {
+        fn run(&self) {
+            self.handler.execute();
+            self.generic_handler.execute();
         }
+    }
 
-        // Add "constructor"'s
-        impl<Age, Email> PersonBuilder<PersonBuilderNameEmpty, Age, Email> {
-            fn name(self, name: String) -> PersonBuilder<PersonBuilderNameAdded, Age, Email> {
-                PersonBuilder {
-                    name: PersonBuilderNameAdded(name),
-                    age: self.age,
-                    email: self.email,
-                }
-            }
+    #[derive(TypestateBuilder)]
+    struct NestedGenerics<'a, T, U>
+    where
+        T: 'a + Copy + Clone,
+        U: 'a + AsRef<T> + Clone,
+    {
+        value: &'a T,
+        ref_container: U,
+    }
+
+    impl<'a, T, U> NestedGenerics<'a, T, U>
+    where
+        T: 'a + Copy + Clone,
+        U: 'a + AsRef<T> + Clone,
+    {
+        fn get_value(&self) -> T {
+            *self.value
         }
-
-        impl<Name, Email> PersonBuilder<Name, PersonBuilderAgeEmpty, Email> {
-            fn age(self, age: u32) -> PersonBuilder<Name, PersonBuilderAgeAdded, Email> {
-                PersonBuilder {
-                    name: self.name,
-                    age: PersonBuilderAgeAdded(age),
-                    email: self.email,
-                }
-            }
-        }
-
-        impl<Name, Age> PersonBuilder<Name, Age, PersonBuilderEmailEmpty> {
-            fn email(
-                self,
-                email: Option<String>,
-            ) -> PersonBuilder<Name, Age, PersonBuilderEmailAdded> {
-                PersonBuilder {
-                    name: self.name,
-                    age: self.age,
-                    email: PersonBuilderEmailAdded(email),
-                }
-            }
-        }
-
-        // Build time
-        impl PersonBuilder<PersonBuilderNameAdded, PersonBuilderAgeAdded, PersonBuilderEmailAdded> {
-            fn build(self) -> Person {
-                Person {
-                    name: self.name.0,
-                    age: self.age.0,
-                    email: self.email.0,
-                }
-            }
-        }
-
-        // Add setter and/or getter method for a field
-        impl Person {
-            fn get_email(&self) -> &Option<String> {
-                &self.email
-            }
-            fn set_email(&mut self, email: Option<String>) {
-                self.email = email;
-            }
-        }
-
-        let builder = Person::builder();
-        println!("{:?}", builder);
-        let builder = builder.name("Alice Johnson".to_string());
-        println!("{:?}", builder);
-        let builder = builder.age(30);
-        println!("{:?}", builder);
-        let builder = builder.email(None);
-        println!("{:?}", builder);
-        let mut person = builder.build();
-        println!("{:?}", person);
-        println!("{:?}", person.get_email());
-        person.set_email(Some("alice@example.com".to_string()));
-        println!("{:?}", person);
     }
 }
